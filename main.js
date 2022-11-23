55012f5870e007660e32f01bd972bbae83dade5c
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-11-23 12:38:11 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+// import ScrollMagic from "scrollmagic"
+// import { ScrollMagicPluginIndicator} from "scrollmagic-plugins";
+// import { TweenMax, TimelineMax } from "gsap";
+// import { ScrollMagicPluginGsap } from "scrollmagic-plugins";
+
+// ScrollMagicPluginIndicator(ScrollMagic);
+// ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
+
+// const controller = new ScrollMagic.Controller();
+// const scene  = new ScrollMagic.Scene( { triggerElement: ".trigger", duration: 800, offset: 450})
+//                 .setTween("#animate", 1, {className: "extended"})
+//                 .addIndicators( { name: "marcador" } )
+//                 .addTo( controller );
