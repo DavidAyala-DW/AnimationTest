945478cb8a640b0670825d05de46100c4d804c6f
DavidAyalaDW <luis.zanabria2@unmsm.edu.pe>
2022-11-24 13:33:10 -0500

    1024

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ let lottieProgress = lottie.loadAnimation({
   path: "Kuru_Shoe_v3.json"
   });
   
-
 const controller = new ScrollMagic.Controller();      
 
 new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 1000, offset: 1000 })
@@ -36,7 +35,6 @@ new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 0, offset: 3000 })
     let scrollFromTop = window.scrollY;
     let totalFrames = lottieProgress.totalFrames;
     let scrollPercentage = (scrollFromTop * 100) / totalHeight;
-    let scrollPercentRounded = Math.round(scrollPercentage); // Just in case
   
     // Check if the current frame is the last frame. If it's the last frame, do nothing. This prevents showing the empty frame at the end. Thanks Pauline for pointing out.
     if ((scrollPercentage * totalFrames) / 100 < totalFrames) {
@@ -49,4 +47,127 @@ new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 0, offset: 3000 })
 
 });
 
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 2000, offset: 4150 })
 
+.setTween("#ultimate_insole", 1, { className: "+=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 2000, offset: 4150 })
+
+.setTween("#kuru_cloud", 1, { className: "+=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 2000, offset: 4150 })
+
+.setTween("#kuru_sole", 1, { className: "+=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+//Content
+
+//Ultimate Insole
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 3200, offset: 4800 })
+
+.setTween("#ultimate_insole", 1, { className: "+=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 4800 })
+
+.setTween("#ultimate_insole_content", 1, { className: "+=opactity-active" })
+.addIndicators({ name: "ultimate_insole_content" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 9000 })
+
+.setTween("#ultimate_insole", 1, { className: "-=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 9000 })
+
+.setTween("#ultimate_insole_content", 1, { className: "-=opactity-active" })
+.addIndicators({ name: "ultimate_insole_content" })
+.addTo(controller);
+
+//kuru_cloud
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 5500, offset: 9000 })
+
+.setTween("#kuru_cloud", 1, { className: "+=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 9000 })
+
+.setTween("#kuru_cloud_content", 1, { className: "+=opactity-active"  })
+.addIndicators({ name: "ultimate_insole_content" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 14300 })
+
+.setTween("#kuru_cloud", 1, { className: "-=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 14300 })
+
+.setTween("#kuru_cloud_content", 1, { className: "-=opactity-active"  })
+.addIndicators({ name: "ultimate_insole_content" })
+.addTo(controller);
+
+//Sole
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 5700, offset: 14300 })
+
+.setTween("#kuru_sole", 1, { className: "+=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 14300 })
+
+.setTween("#kuru_sole_content", 1, { className: "+=opactity-active"  })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 0, offset: 20000 })
+
+.setTween("#kuru_sole", 1, { className: "-=active_card" })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 200, offset: 19800 })
+
+.setTween("#kuru_sole_content", 1, { className: "-=opactity-active"  })
+.addIndicators({ name: "ultimate_insole" })
+.addTo(controller);
+
+
+//Clear All
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 500, offset: 20000 })
+
+.setTween("#ultimate_insole", 1, { className: "-=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 500, offset: 20000 })
+
+.setTween("#kuru_cloud", 1, { className: "-=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 500, offset: 20000 })
+
+.setTween("#kuru_sole", 1, { className: "-=opactity-active" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
+
+new ScrollMagic.Scene({ triggerElement: ".trigger", duration: 500, offset: 20000 })
+
+.setTween("#animate", 1, { className: "+=opactity-disabled" })
+.addIndicators({ name: "marcador3" })
+.addTo(controller);
